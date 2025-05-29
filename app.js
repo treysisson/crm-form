cb98028e6492e55f9836950c8e1bbc57d5133125
treysisson <trey.sisson@gmail.com>
2025-05-28 20:08:45 -0700

    Update app.js

--- app.js
+++ app.js
@@ -132,7 +132,8 @@ document.getElementById('searchBtn').addEventListener('click', async () => {
         const submitBtn = document.createElement('button');
         submitBtn.textContent = 'Submit';
         submitBtn.type = 'submit';
-        submitBtn.className = 'mt-4 bg-[var(--color-secondary)] text-black px-4 py-2 rounded hover:opacity-90 disabled:opacity-50';
+        submitBtn.className = 'mt-4 text-black px-4 py-2 rounded hover:opacity-90 disabled:opacity-50';
+        submitBtn.style.backgroundColor = '#ffff8f';
 
         form.appendChild(submitBtn);
 
